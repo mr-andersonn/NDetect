2290341ed719050fb287796715c83b5acafea9fb
Anton <bogatchuk.anton@gmail.com>
2025-05-27 03:01:35 +0200

    Text Style Update

--- NetSniffer/obj/Debug/net9.0/NetSniffer.AssemblyInfo.cs
+++ NetSniffer/obj/Debug/net9.0/NetSniffer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetSniffer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a62f405927d882dcadcfe5735bb65b8e1336c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef0128e8ca51e206540f842d20bf4ba134366a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetSniffer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetSniffer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
