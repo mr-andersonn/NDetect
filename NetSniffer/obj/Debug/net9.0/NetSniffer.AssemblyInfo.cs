45f553f44225087665909c363882a8fdd97c1459
neo <bogatchuk.anton@gmail.com>
2025-05-28 14:22:39 +0200

    Added CommandRunner Class, ArpTableReader partually implemented

--- NetSniffer/obj/Debug/net9.0/NetSniffer.AssemblyInfo.cs
+++ NetSniffer/obj/Debug/net9.0/NetSniffer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetSniffer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1b843453306c401791ff07dba2bda55b36a360")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70408f4f8d0575a3fefb07635bfd22cdc4b53ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetSniffer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetSniffer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
